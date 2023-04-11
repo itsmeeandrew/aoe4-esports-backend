735732073e3c6708c22ffdb6d3b727428c1863d3
itsmeeandrew <schin.andras01@gmail.com>
2023-04-11 15:38:14 +0200

    Initial commit

---
+++ src/main/kotlin/net/itsmeeandrew/aoe4esports/AgeOfEmpires4EsportsApplication.kt
@@ -0,0 +1,11 @@
+package net.itsmeeandrew.aoe4esports
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AgeOfEmpires4EsportsApplication
+
+fun main(args: Array<String>) {
+	runApplication<AgeOfEmpires4EsportsApplication>(*args)
+}
---
+++ src/test/kotlin/net/itsmeeandrew/aoe4esports/AgeOfEmpires4EsportsApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.itsmeeandrew.aoe4esports
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AgeOfEmpires4EsportsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
