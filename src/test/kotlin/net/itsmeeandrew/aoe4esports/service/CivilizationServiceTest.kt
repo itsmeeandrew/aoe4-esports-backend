6b02b048b04528711eb4a57c15e6dce0f5d88f07
itsmeeandrew <schin.andras01@gmail.com>
2023-06-08 16:35:57 +0200

    Use test database in Civilization tests

--- src/test/kotlin/net/itsmeeandrew/aoe4esports/service/CivilizationServiceTest.kt
+++ src/test/kotlin/net/itsmeeandrew/aoe4esports/service/CivilizationServiceTest.kt
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
 
 @SpringBootTest
+@TestPropertySource(
+    locations = ["classpath:application-test.properties"]
+)
 class CivilizationServiceTest(
     @Autowired val civilizationService: CivilizationService
 ) {
